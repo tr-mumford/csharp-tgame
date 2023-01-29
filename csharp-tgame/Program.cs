447933cf7699d29ed43d49c506cad12d26c330bd
tr-mumford <tyler.r.mumford@gmail.com>
2023-01-29 22:20:28 +0200

    First basic code commit.

---
+++ csharp-tgame/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading;
+using System.Windows.Input;
+
+namespace csharp_tgame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(2, 2);
+            Console.CursorVisible = false;
+
+            var countP = 0;
+            var countE = 26;
+
+            while (true)
+            {
+                char[] land =
+                {
+                    '_', '_', '_', '_', '_', '_', '_', '_',
+                    '_', '_', '_', '_', '_', '_', '_', '_',
+                    '_', '_', '_', '_', '_', '_', '_', '_',
+                    '_', '_', '_', '_', '_', '_', '_', '_',
+                };
+
+
+                for (int i = 0; i < land.Length; i++)
+                {
+                    land[countP] = 'X';
+                    land[countE] = '<';
+                    Console.Write(land[i]);
+                }
+
+                Console.SetCursorPosition(2, 2);
+
+                var testString = Console.ReadKey().KeyChar;
+                if (testString == 'j') { countP--; }
+                if (testString == 'k') { countP++; }
+                countE = countE - 2;
+
+                if(countE == countP)
+                {
+                    Console.WriteLine("DEATH");
+                    Console.WriteLine("DEATH");
+                    Console.WriteLine("DEATH");
+                    Console.WriteLine("DEATH");
+                    Console.WriteLine("DEATH");
+                    Console.WriteLine("DEATH");
+                    Console.WriteLine("DEATH");
+                    Console.WriteLine("DEATH");
+                    Console.WriteLine("DEATH");
+                    Console.WriteLine("DEATH");
+                    Console.WriteLine("DEATH");
+                }
+            }
+        }
+    }
+}
